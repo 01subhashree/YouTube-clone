0322169ad55094695163fd856fb1ab6221f9de20
01subhashree/ <ssubhashree094@gmail.com>
2023-09-26 12:36:54 +0530

    small changes

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -3,16 +3,20 @@ import VideoCard from "../videoCard/VideoCard";
 import style from "./Video.module.css";
 
 export default function Videos({ videos, isHome }) {
-  console.log(videos);
-  console.log(isHome);
   return (
     <div className={isHome ? style.VideoCard : style.videoContainer}>
       {videos &&
-        videos.map((ele, index) => (
-          <div key={index} className={style.videoCardContainer}>
-            {ele.id.videoId && <VideoCard video={ele} />}
-          </div>
-        ))}
+        videos.map((video, index) => {
+          if (video.id && video.id.videoId) {
+            return (
+              <div key={index} className={style.videoCardContainer}>
+                <VideoCard video={video} />
+              </div>
+            );
+          } else {
+            return null; // Don't render anything if videoId is missing
+          }
+        })}
     </div>
   );
 }
